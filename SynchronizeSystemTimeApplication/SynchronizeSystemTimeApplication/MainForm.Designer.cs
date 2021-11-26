46fcead671fe1b0d1027780aa9e0f7090043ff8c
Thanh Tung <thanhtung28400@gmail.com>
2021-11-26 14:12:52 +0700

    Archive this shit

---
+++ SynchronizeSystemTimeApplication/SynchronizeSystemTimeApplication/MainForm.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace SynchronizeSystemTimeApplication
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.buttonSaveChanges = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxTimeZone = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSync = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxTimeServer = new System.Windows.Forms.ComboBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current date and time";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Checked = false;
+            this.dateTimePicker.CustomFormat = "MM/dd/yyyy  hh:mm:ss tt";
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(28, 56);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(462, 30);
+            this.dateTimePicker.TabIndex = 1;
+            // 
+            // buttonSaveChanges
+            // 
+            this.buttonSaveChanges.Location = new System.Drawing.Point(509, 53);
+            this.buttonSaveChanges.Name = "buttonSaveChanges";
+            this.buttonSaveChanges.Size = new System.Drawing.Size(150, 33);
+            this.buttonSaveChanges.TabIndex = 2;
+            this.buttonSaveChanges.Text = "Save changes";
+            this.buttonSaveChanges.UseVisualStyleBackColor = true;
+            this.buttonSaveChanges.Click += new System.EventHandler(this.buttonSaveChanges_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Time zone";
+            // 
+            // comboBoxTimeZone
+            // 
+            this.comboBoxTimeZone.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTimeZone.FormattingEnabled = true;
+            this.comboBoxTimeZone.Location = new System.Drawing.Point(28, 300);
+            this.comboBoxTimeZone.Name = "comboBoxTimeZone";
+            this.comboBoxTimeZone.Size = new System.Drawing.Size(631, 33);
+            this.comboBoxTimeZone.TabIndex = 4;
+            this.comboBoxTimeZone.SelectedIndexChanged += new System.EventHandler(this.comboBoxTimeZone_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(501, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Synchronize your clock with an Internet time server";
+            // 
+            // buttonSync
+            // 
+            this.buttonSync.Location = new System.Drawing.Point(28, 202);
+            this.buttonSync.Name = "buttonSync";
+            this.buttonSync.Size = new System.Drawing.Size(167, 44);
+            this.buttonSync.TabIndex = 6;
+            this.buttonSync.Text = "Sync now";
+            this.buttonSync.UseVisualStyleBackColor = true;
+            this.buttonSync.Click += new System.EventHandler(this.buttonSync_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(91, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Time server";
+            // 
+            // comboBoxTimeServer
+            // 
+            this.comboBoxTimeServer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTimeServer.FormattingEnabled = true;
+            this.comboBoxTimeServer.Items.AddRange(new object[] {
+            "time.windows.com",
+            "time-a-g.nist.gov",
+            "time.apple.com",
+            "pool.ntp.org",
+            "1.amazon.pool.ntp.org"});
+            this.comboBoxTimeServer.Location = new System.Drawing.Point(206, 152);
+            this.comboBoxTimeServer.Name = "comboBoxTimeServer";
+            this.comboBoxTimeServer.Size = new System.Drawing.Size(402, 33);
+            this.comboBoxTimeServer.TabIndex = 8;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(686, 364);
+            this.Controls.Add(this.comboBoxTimeServer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonSync);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxTimeZone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonSaveChanges);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Synchronize Time";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button buttonSaveChanges;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxTimeZone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonSync;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxTimeServer;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ SynchronizeSystemTimeApplication/SynchronizeSystemTimeApplication/MainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+using Time;
+
+namespace SynchronizeSystemTimeApplication
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            comboBoxTimeServer.SelectedItem = "time.windows.com";
+            comboBoxTimeZone.SelectedIndexChanged -= new EventHandler(comboBoxTimeZone_SelectedIndexChanged);
+            comboBoxTimeZone.DataSource = TimeZoneInfo.GetSystemTimeZones();
+            comboBoxTimeZone.DisplayMember = "DisplayName";
+            comboBoxTimeZone.ValueMember = "Id";
+            comboBoxTimeZone.SelectedValue = TimeZone.CurrentTimeZone.StandardName;
+            comboBoxTimeZone.SelectedIndexChanged += new EventHandler(comboBoxTimeZone_SelectedIndexChanged);
+        }
+
+        private void buttonSaveChanges_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SystemTime.SetSystemDateTime(dateTimePicker.Value.ToString("MM/dd/yyyy"), dateTimePicker.Value.ToString("hh:mm:ss tt"));
+            }
+            catch
+            {
+                MessageBox.Show(this, "Error to change date and time of your system!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonSync_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime dt = SystemTime.GetNetworkTime(comboBoxTimeServer.SelectedItem.ToString());
+                dateTimePicker.Value = dt;
+                SystemTime.SetSystemDateTime(dateTimePicker.Value.ToString("MM/dd/yyyy"), dateTimePicker.Value.ToString("hh:mm:ss tt"));
+            }
+            catch
+            {
+                MessageBox.Show(this, "Error to change date and time of your system!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            dateTimePicker.Value = dateTimePicker.Value.AddSeconds(1);
+        }
+
+        private void comboBoxTimeZone_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                SystemTime.SetSystemTimeZone(comboBoxTimeZone.SelectedValue.ToString());
+                dateTimePicker.Value = DateTime.Now;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
